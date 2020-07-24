7095ed734f2437e51bc4f2ed6fa33163987c420c
Bryant Vail <BryantVail@gmail.com>
2020-07-24 09:16:15 -0400

    10:06/45:02

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # HttpClient_IAmCorey_2020-July
  HttpClient_IAmCorey_2020-July
+
+ {
+
+    packages: [
+     {
+      name: "System.Net.Http.Json", 
+      description: "namespace used to more easily deal with 'json' in the realm of api requests & responses"
+	 }
+	]
+     exampleNotes: [
+      {
+       url: "https://www.metaWeather.com/api/location/{id}",
+	  }
+	 ]
+ }
+
+
+
+
+
+
+
+
+
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace HttpClient_IAmCorey_2020_July
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient(); //httpClient factory thats available to us
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
