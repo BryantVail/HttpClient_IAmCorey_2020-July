0f2524904cfff97d4ce5a0c3cbde0d5eaf63b0c7
Bryant Vail <BryantVail@gmail.com>
2020-08-16 17:38:56 -0400

    completed video

---
+++ HttpClient_IAmCorey_2020-July/Models/DayForecastModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HttpClient_IAmCorey_2020_July.Models
+{
+    public class DayForecastModel
+    {
+        public string Weather_state_name { get; set; }
+        public string Applicable_date { get; set; }
+        public float Min_temp { get; set; }
+        public float Max_temp { get; set; }
+        //public float the_temp { get; set; }
+        //public float wind_speed { get; set; }
+        //public float wind_direction { get; set; }
+        //public float air_pressure { get; set; }
+        //public int humidity { get; set; }
+        //public float visibility { get; set; }
+        //public int predictability { get; set; }
+    }
+
+    
+
+
+}
---
+++ HttpClient_IAmCorey_2020-July/Models/WeatherForecastModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClient_IAmCorey_2020_July.Models
+{
+
+
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; } // sunRise (Json) => SunRise (C#)
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+
+    
+
+
+}
